ae133afd670e401b600e9b9ccf6f4a0c6916e14a
Sergey Tikhonov <166549676+TraseRRR98@users.noreply.github.com>
2026-06-02 17:37:13 -0400

    Add language dropdown to LanguageSelector
    Expand Chakra UI imports and replace the static label with a dropdown Menu in LanguageSelector. Renders a MenuButton (showing 'Python') and a MenuList with language options (Python, C++, PHP, Java) while keeping the existing 'Language:' label to allow selecting a programming language.

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,9 +1,19 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Menu, Button, MenuButton, MenuItem, MenuList, MenuItemGroup, MenuSeparator, MenuArrow, MenuCheckboxItem, MenuRadioItem, MenuRadioGroup } from '@chakra-ui/react';
+
 
 const LanguageSelector = () => {
     return (
         <Box>
             <Text mb={2} fontSize="lg" color="gray.500">Language: </Text>
+            <Menu>
+                <MenuButton as={Button}>Python</MenuButton>
+                <MenuList>
+                    <MenuItem>Python</MenuItem>
+                    <MenuItem>C++</MenuItem>
+                    <MenuItem>PHP</MenuItem>
+                    <MenuItem>Java</MenuItem>
+                </MenuList>
+            </Menu>
         </Box>
     )
 }
